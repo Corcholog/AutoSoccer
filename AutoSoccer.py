396dd7f04babd0ab7b2bfad57d33d2d2366ee58b
Corcho <loguerciogiorgioivan@gmail.com>
2024-01-11 16:50:30 -0300

    Signed-off-by: Corcho <loguerciogiorgioivan@gmail.com>

--- AutoSoccer.py
+++ AutoSoccer.py
@@ -34,115 +34,191 @@ area_padding2 = area_padding1 + (7.5625 * goal_area_height)/85
 
 the_capricious = (screen_width * screen_height) / 110000
 
-FIELD_TOP_LEFT = [screen_width-field_width,screen_height-field_height]
-FIELD_TOP_RIGHT = [field_width,screen_height-field_height]
-FIELD_BOTTOM_LEFT = [screen_width-field_width,field_height]
-FIELD_BOTTOM_RIGHT = [field_width,field_height]
+
 
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-#pygame.mixer.music.load("bg_music.mp3")
-#pygame.mixer.music.play()
+ball_img = pygame.image.load("src/img/ball.png")
+
+class Ball:
+    def __init__(self, pos: list[float]) -> None:
+        self.speed = [0,0]
+        self.x = pos[0]
+        self.y = pos[1]
+        self.last_touch = None
+        self.image = ball_img
+        self.scaled_ball = pygame.transform.scale(ball_img, (50, 50)) # to-do: rescalar dinamicamente
+        self.img_rect = self.scaled_ball.get_rect(center=(self.x, self.y))
+
+    def reposition(self, pos: list[float]) -> None:
+        self.x = pos[0]
+        self.y = pos[1]
+        self.img_rect.topleft = (self.x, self.y)
+
+    def draw(self) -> None:
+        screen.blit(self.scaled_ball, self.img_rect)
+    
+    def reset_speed(self) -> None:
+        self.speed = [0,0]
+
 
 class SoccerField:
-    def draw(self) -> None:
-        # center circle
-        pygame.draw.circle(screen, WHITE, [screen_width/2,screen_height/2], penalty_area_width/2, grosor)
+    def __init__(self, players) -> None:
+        self.μ = 5 # ???
+        self.players = players
+        self.center_circle = [screen_width/2,screen_height/2]
+        self.middle_line = ([screen_width/2, screen_height-field_height], [screen_width/2, field_height])
+        self.upper_sideline = ([screen_width-field_width,field_height], [field_width,field_height])
+        self.bottom_sideline = ([screen_width-field_width,screen_height-field_height], [field_width,screen_height-field_height])
+        self.left_endline = ([field_width, screen_height-field_height], [field_width, field_height])
+        self.right_endline = ([screen_width-field_width, screen_height-field_height], [screen_width-field_width, field_height])
+        self.ball_initial_pos = [screen_width/2,screen_height/2]
+        self.ball = Ball(self.ball_initial_pos)
+        self.top_left_corner = [screen_width-field_width,screen_height-field_height]
+        self.top_right_corner = [field_width,screen_height-field_height]
+        self.bottom_left_corner = [screen_width-field_width,field_height]
+        self.bottom_right_corner = [field_width,field_height]
 
-        # half field band
-        pygame.draw.line(screen, WHITE, [screen_width/2, screen_height-field_height], [screen_width/2, field_height], grosor)
+        self.ls_penalty_area_upper = ([screen_width-field_width, screen_height-field_height+area_padding], [screen_width-field_width+penalty_area_width,screen_height-field_height+area_padding])
+        self.ls_penalty_area_bottom = ([screen_width-field_width, field_height-area_padding], [screen_width-field_width+penalty_area_width,field_height-area_padding])
+        self.ls_penalty_area_singleline = ([screen_width-field_width+penalty_area_width, screen_height-field_height+area_padding], [screen_width-field_width+penalty_area_width,field_height-area_padding])
+        self.ls_goal_area_upper = ([screen_width-field_width,  screen_height-field_height+area_padding1], [screen_width-field_width+goal_area_width,  screen_height-field_height+area_padding1])
+        self.ls_goal_area_bottom = ([screen_width-field_width,  screen_height-field_height+area_padding+penalty_area_height-area_padding1], [screen_width-field_width+goal_area_width, screen_height-field_height+area_padding+penalty_area_height-area_padding1])
+        self.ls_goal_area_singleline = ([screen_width-field_width+goal_area_width,screen_height-field_height+area_padding1], [screen_width-field_width+goal_area_width,screen_height-field_height+area_padding+penalty_area_height-area_padding1])
+        self.ls_arco_area_upper = ([screen_width-field_width,  screen_height-field_height+area_padding2], [screen_width-field_width-arco_ancho,  screen_height-field_height+area_padding2])
+        self.ls_arco_area_bottom = ([screen_width-field_width, screen_height-field_height+area_padding+penalty_area_height-area_padding2], [screen_width-field_width-arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2])
+        self.ls_arco_area_singlelane = ([screen_width-field_width-arco_ancho, screen_height-field_height+area_padding2], [screen_width-field_width-arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2])
+        self.ls_penalty_box_arc = [screen_width-field_width+penalty_area_width, screen_height/2]
+        self.ls_penalty_kick_mark = [screen_width-field_width+goal_area_width+((penalty_area_width-goal_area_width)/2), screen_height/2]
+
+        self.rs_penalty_area_upper = ([field_width, screen_height-field_height+area_padding], [field_width-penalty_area_width,screen_height-field_height+area_padding])
+        self.rs_penalty_area_bottom = ([field_width, field_height-area_padding], [field_width-penalty_area_width,field_height-area_padding])
+        self.rs_penalty_area_singleline = ([field_width-penalty_area_width, screen_height-field_height+area_padding], [field_width-penalty_area_width,field_height-area_padding])
+        self.rs_goal_area_upper = ([field_width,  screen_height-field_height+area_padding1], [field_width-goal_area_width,  screen_height-field_height+area_padding1])
+        self.rs_goal_area_bottom = ([field_width,  screen_height-field_height+area_padding+penalty_area_height-area_padding1], [field_width-goal_area_width, screen_height-field_height+area_padding+penalty_area_height-area_padding1])
+        self.rs_goal_area_singleline = ([field_width-goal_area_width,screen_height-field_height+area_padding1], [field_width-goal_area_width,screen_height-field_height+area_padding+penalty_area_height-area_padding1])
+        self.rs_arco_area_upper = ([field_width,  screen_height-field_height+area_padding2], [field_width+arco_ancho,  screen_height-field_height+area_padding2])
+        self.rs_arco_area_bottom = ([field_width, screen_height-field_height+area_padding+penalty_area_height-area_padding2], [field_width+arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2])
+        self.rs_arco_area_singlelane = ([field_width+arco_ancho, screen_height-field_height+area_padding2], [field_width+arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2])
+        self.rs_penalty_box_arc = [field_width-penalty_area_width, screen_height/2]
+        self.rs_penalty_kick_mark = [field_width-goal_area_width-((penalty_area_width-goal_area_width)/2), screen_height/2]
+
+    def draw(self) -> None:
+
+        
+
+        # center circle
+        pygame.draw.circle(screen, WHITE, self.center_circle, penalty_area_width/2, grosor)
+
+        # middlefield band
+        pygame.draw.line(screen, WHITE, self.middle_line[0], self.middle_line[1], grosor)
 
         # field sidelines
-        pygame.draw.line(screen, WHITE, [screen_width-field_width,field_height], [field_width,field_height], grosor)
-        pygame.draw.line(screen, WHITE, [screen_width-field_width,screen_height-field_height], [field_width,screen_height-field_height], grosor)
+        #pygame.draw.line(screen, WHITE, self.upper_sideline[0], self.upper_sideline[1], grosor)
+        pygame.draw.line(screen, WHITE, self.bottom_sideline[0], self.bottom_sideline[1], grosor)
 
         # field endlines
-        pygame.draw.line(screen, WHITE, [field_width, screen_height-field_height], [field_width, field_height], grosor)
-        pygame.draw.line(screen, WHITE, [screen_width-field_width, screen_height-field_height], [screen_width-field_width, field_height], grosor)
+        pygame.draw.line(screen, WHITE, self.left_endline[0], self.left_endline[1], grosor)
+        pygame.draw.line(screen, WHITE, self.right_endline[0], self.right_endline[1], grosor)
 
-        # ball
-        pygame.draw.circle(screen, WHITE, [screen_width/2,screen_height/2], the_capricious)
+       
 
         # LEFT SIDE
 
         # penalty area
 
-        pygame.draw.line(screen, (203,203,203), [screen_width-field_width, screen_height-field_height+area_padding], [screen_width-field_width+penalty_area_width,screen_height-field_height+area_padding], grosor)
-        pygame.draw.line(screen, (203,203,203), [screen_width-field_width, field_height-area_padding], [screen_width-field_width+penalty_area_width,field_height-area_padding], grosor)
+        pygame.draw.line(screen, (203,203,203), self.ls_penalty_area_upper[0], self.ls_penalty_area_upper[1], grosor)
+        pygame.draw.line(screen, (203,203,203), self.ls_penalty_area_bottom[0], self.ls_penalty_area_bottom[1], grosor)
 
-        pygame.draw.line(screen, (203,203,203), [screen_width-field_width+penalty_area_width, screen_height-field_height+area_padding], [screen_width-field_width+penalty_area_width,field_height-area_padding], grosor)
+        pygame.draw.line(screen, (203,203,203), self.ls_penalty_area_singleline[0], self.ls_penalty_area_singleline[1], grosor)
 
 
         # goal area
-        pygame.draw.line(screen, (166,166,166), [screen_width-field_width,  screen_height-field_height+area_padding1], [screen_width-field_width+goal_area_width,  screen_height-field_height+area_padding1], grosor)
-        pygame.draw.line(screen, (166,166,166), [screen_width-field_width,  screen_height-field_height+area_padding+penalty_area_height-area_padding1], [screen_width-field_width+goal_area_width, screen_height-field_height+area_padding+penalty_area_height-area_padding1], grosor)
+        pygame.draw.line(screen, (166,166,166), self.ls_goal_area_upper[0], self.ls_goal_area_upper[1], grosor)
+        pygame.draw.line(screen, (166,166,166), self.ls_goal_area_bottom[0], self.ls_goal_area_bottom[1], grosor)
 
-        pygame.draw.line(screen,  (166,166,166), [screen_width-field_width+goal_area_width,screen_height-field_height+area_padding1], [screen_width-field_width+goal_area_width,screen_height-field_height+area_padding+penalty_area_height-area_padding1], grosor)
+        pygame.draw.line(screen,  (166,166,166), self.ls_goal_area_singleline[0], self.ls_goal_area_singleline[1], grosor)
 
 
         # arco 
-        pygame.draw.line(screen, (129,129,129), [screen_width-field_width,  screen_height-field_height+area_padding2], [screen_width-field_width-arco_ancho,  screen_height-field_height+area_padding2], grosor2)
-        pygame.draw.line(screen, (129,129,129), [screen_width-field_width, screen_height-field_height+area_padding+penalty_area_height-area_padding2], [screen_width-field_width-arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2], grosor2)
+        pygame.draw.line(screen, (129,129,129), self.ls_arco_area_upper[0], self.ls_arco_area_upper[1], grosor2)
+        pygame.draw.line(screen, (129,129,129), self.ls_arco_area_bottom[0], self.ls_arco_area_bottom[1], grosor2)
 
-        pygame.draw.line(screen, (129,129,129), [screen_width-field_width-arco_ancho, screen_height-field_height+area_padding2], [screen_width-field_width-arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2], grosor)
+        pygame.draw.line(screen, (129,129,129), self.ls_arco_area_singlelane[0], self.ls_arco_area_singlelane[1], grosor)
 
         # penalty box arc
-        pygame.draw.circle(screen, (221,221,221), [screen_width-field_width+penalty_area_width, screen_height/2], penalty_area_width/2, grosor, True, False, False, True)
+        pygame.draw.circle(screen, (221,221,221), self.ls_penalty_box_arc, penalty_area_width/2, grosor, True, False, False, True)
 
         # penalty kick mark
-        pygame.draw.circle(screen, (0,0,0), [screen_width-field_width+goal_area_width+((penalty_area_width-goal_area_width)/2), screen_height/2], grosor, grosor)
+        pygame.draw.circle(screen, (0,0,0), self.ls_penalty_kick_mark, grosor, grosor)
 
         # corner marks
-        pygame.draw.circle(screen, WHITE, FIELD_TOP_LEFT, int(goal_area_width/2), grosor, False, False, False, True)
-        pygame.draw.circle(screen, WHITE, FIELD_TOP_RIGHT, int(goal_area_width/2), grosor, False, False, True, False)
-        pygame.draw.circle(screen, WHITE, FIELD_BOTTOM_LEFT, int(goal_area_width/2), grosor, True, False, False, False)
-        pygame.draw.circle(screen, WHITE, FIELD_BOTTOM_RIGHT, int(goal_area_width/2), grosor, False, True, False, False)
+        pygame.draw.circle(screen, WHITE, self.top_left_corner, int(goal_area_width/2), grosor, False, False, False, True)
+        pygame.draw.circle(screen, WHITE, self.top_right_corner, int(goal_area_width/2), grosor, False, False, True, False)
+        pygame.draw.circle(screen, WHITE, self.bottom_left_corner, int(goal_area_width/2), grosor, True, False, False, False)
+        pygame.draw.circle(screen, WHITE, self.bottom_right_corner, int(goal_area_width/2), grosor, False, True, False, False)
 
 
         # RIGHT SIDE
 
         # penalty area
 
-        pygame.draw.line(screen, (203,203,203), [field_width, screen_height-field_height+area_padding], [field_width-penalty_area_width,screen_height-field_height+area_padding], grosor)
-        pygame.draw.line(screen, (203,203,203), [field_width, field_height-area_padding], [field_width-penalty_area_width,field_height-area_padding], grosor)
+        pygame.draw.line(screen, (203,203,203), self.rs_penalty_area_upper[0], self.rs_penalty_area_upper[1], grosor)
+        pygame.draw.line(screen, (203,203,203), self.rs_penalty_area_bottom[0], self.rs_penalty_area_bottom[1], grosor)
 
-        pygame.draw.line(screen, (203,203,203), [field_width-penalty_area_width, screen_height-field_height+area_padding], [field_width-penalty_area_width,field_height-area_padding], grosor)
+        pygame.draw.line(screen, (203,203,203), self.rs_penalty_area_singleline[0], self.rs_penalty_area_singleline[1], grosor)
 
         # goal area
-        pygame.draw.line(screen, (166,166,166), [field_width,  screen_height-field_height+area_padding1], [field_width-goal_area_width,  screen_height-field_height+area_padding1], grosor)
-        pygame.draw.line(screen, (166,166,166), [field_width,  screen_height-field_height+area_padding+penalty_area_height-area_padding1], [field_width-goal_area_width, screen_height-field_height+area_padding+penalty_area_height-area_padding1], grosor)
+        pygame.draw.line(screen, (166,166,166), self.rs_goal_area_upper[0], self.rs_goal_area_upper[1], grosor)
+        pygame.draw.line(screen, (166,166,166), self.rs_goal_area_bottom[0], self.rs_goal_area_bottom[1], grosor)
 
-        pygame.draw.line(screen,  (166,166,166), [field_width-goal_area_width,screen_height-field_height+area_padding1], [field_width-goal_area_width,screen_height-field_height+area_padding+penalty_area_height-area_padding1], grosor)
+        pygame.draw.line(screen,  (166,166,166), self.rs_goal_area_singleline[0], self.rs_goal_area_singleline[1], grosor)
 
         # arco 
-        pygame.draw.line(screen, (129,129,129), [field_width,  screen_height-field_height+area_padding2], [field_width+arco_ancho,  screen_height-field_height+area_padding2], grosor2)
-        pygame.draw.line(screen, (129,129,129), [field_width, screen_height-field_height+area_padding+penalty_area_height-area_padding2], [field_width+arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2], grosor2)
+        pygame.draw.line(screen, (129,129,129), self.rs_arco_area_upper[0], self.rs_arco_area_upper[1], grosor2)
+        pygame.draw.line(screen, (129,129,129), self.rs_arco_area_bottom[0], self.rs_arco_area_bottom[1], grosor2)
 
-        pygame.draw.line(screen, (129,129,129), [field_width+arco_ancho, screen_height-field_height+area_padding2], [field_width+arco_ancho, screen_height-field_height+area_padding+penalty_area_height-area_padding2], grosor)
+        pygame.draw.line(screen, (129,129,129), self.rs_arco_area_singlelane[0], self.rs_arco_area_singlelane[1], grosor)
 
         # penalty kick mark
-        pygame.draw.circle(screen, (0,0,0), [field_width-goal_area_width-((penalty_area_width-goal_area_width)/2), screen_height/2], grosor, grosor)
+        pygame.draw.circle(screen, (0,0,0), self.rs_penalty_kick_mark, grosor, grosor)
 
         # penalty box arc
-        pygame.draw.circle(screen, (221,221,221), [field_width-penalty_area_width, screen_height/2], penalty_area_width/2, grosor, False, True, True, False)
+        pygame.draw.circle(screen, (221,221,221), self.rs_penalty_box_arc, penalty_area_width/2, grosor, False, True, True, False)
+        
+        self.ball.draw()
 
-player_1 = pygame.image.load("PLAYER_1.png")
-scaled_player = pygame.transform.scale(player_1, (100,100))
+    def goal(self) -> None:
+        # WIP
+        return
+
+    def throw_in(self) -> None:
+
+        if self.ball.y > self.upper_sideline[0][1]:
+            self.ball.reposition([self.ball.x, self.upper_sideline[0][1]])
+            self.ball.reset_speed()
+            # mandar jugador atras de la pelota y hacer player.throw_in()
+
+        elif self.ball.y < self.bottom_sideline[0][1]:
+            self.ball.reposition([self.ball.x, self.bottom_sideline[0][1]])
+            self.ball.reset_speed()
+            # mandar jugador atras de la pelota y hacer player.throw_in()
+        return
+    
+    def corner(self) -> None:
+        
+        return
+field = SoccerField(15)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     
     screen.fill(GREEN)
-    field = SoccerField()
     field.draw()
-    screen.blit(scaled_player, [screen_width/3, screen_height/3])
-    screen.blit(scaled_player, [screen_width/3, 2*screen_height/3])
-    screen.blit(scaled_player, [screen_width/3, screen_height/2])
-    screen.blit(scaled_player, [2*screen_width/3, screen_height/3])
-    screen.blit(scaled_player, [2*screen_width/3, 2*screen_height/3])
-    screen.blit(scaled_player, [2*screen_width/3, screen_height/2])
+    field.throw_in()
     clock.tick(60)  # limits FPS to 60
     pygame.display.flip()
